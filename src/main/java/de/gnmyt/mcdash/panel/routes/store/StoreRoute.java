0d88bd8bb82565a3c1da218041fcb6c46b7a5f42
Dugu4090 <amdryze2025@gmail.com>
2026-01-03 13:19:19 +0000

    Plugin store: show all plugins (including external), save with actual name

--- src/main/java/de/gnmyt/mcdash/panel/routes/store/StoreRoute.java
+++ src/main/java/de/gnmyt/mcdash/panel/routes/store/StoreRoute.java
@@ -38,9 +38,10 @@ public class StoreRoute extends DefaultHandler {
                 + URLEncoder.encode(query, StandardCharsets.UTF_8.toString()).replace("+", "%20");
 
         HttpUrl url = HttpUrl.parse(ROOT_URL + base).newBuilder()
-                .addQueryParameter("size", "35")
+                .addQueryParameter("size", "100")
                 .addQueryParameter("page", String.valueOf(page))
                 .addQueryParameter("sort", "-downloads")
+                .addQueryParameter("type", "plugin")
                 .build();
 
         okhttp3.Response httpResponse = client.newCall(new okhttp3.Request.Builder().url(url).build()).execute();
@@ -53,14 +54,32 @@ public class StoreRoute extends DefaultHandler {
         ArrayBuilder items = new ArrayBuilder();
 
         mapper.readTree(httpResponse.body().string()).forEach(item -> {
-            if (!item.get("external").asBoolean() && item.get("file").get("type").asText().equals(".jar"))
+            // Include both external and non-external plugins that are JAR files
+            String fileType = item.has("file") && item.get("file").has("type") ? item.get("file").get("type").asText() : "";
+            if (fileType.equals(".jar")) {
+                String icon = null;
+                if (item.has("icon") && item.get("icon").has("data")) {
+                    icon = item.get("icon").get("data").asText();
+                    if (icon.isEmpty()) icon = null;
+                }
+                
+                String description = "";
+                if (item.has("tag")) description = item.get("tag").asText();
+                else if (item.has("description")) description = item.get("description").asText();
+                
+                String version = "1.0";
+                if (item.has("version")) version = item.get("version").asText();
+                else if (item.has("currentVersion")) version = item.get("currentVersion").asText();
+
                 new NodeBuilder(items).add("id", item.get("id").asInt())
                         .add("name", item.get("name").asText())
-                        .add("description", item.get("tag").asText())
-                        .add("icon", !item.get("icon").get("data").asText().isEmpty()
-                                ? item.get("icon").get("data").asText() : null)
-                        .add("downloads", item.get("downloads").asInt())
+                        .add("description", description)
+                        .add("icon", icon)
+                        .add("downloads", item.has("downloads") ? item.get("downloads").asInt() : 0)
+                        .add("version", version)
+                        .add("external", item.has("external") && item.get("external").asBoolean())
                         .register();
+            }
         });
 
         response.type(ContentType.JSON).text(items.toJSON());
@@ -82,6 +101,7 @@ public class StoreRoute extends DefaultHandler {
 
         JsonNode node = mapper.readTree(httpResponse.body().string());
         String projectId = node.get("id").asText();
+        String pluginName = node.get("name").asText();
 
         if (!node.get("file").get("type").asText().equals(".jar")) {
             response.code(400).message("The item with the id '" + request.getQuery().get("id") + "' is not a plugin");
@@ -91,24 +111,28 @@ public class StoreRoute extends DefaultHandler {
         String fileUrl = ROOT_URL + "resources/" + URLEncoder.encode(request.getQuery().get("id"), UTF_8.toString())
                 + "/download";
 
-        if (new File("plugins//Managed-" + projectId + ".jar").exists()) {
-            response.code(409).message("The item with the id '" + projectId + "' is already installed");
+        // Create safe filename from plugin name
+        String safeFileName = pluginName.replaceAll("[^a-zA-Z0-9]", "_") + ".jar";
+        File targetFile = new File("plugins/" + safeFileName);
+
+        if (targetFile.exists()) {
+            response.code(409).message("Plugin '" + pluginName + "' is already installed");
             return;
         }
 
         FileUtils.copyInputStreamToFile(client.newCall(new okhttp3.Request.Builder().url(fileUrl).build())
-                .execute().body().byteStream(), new File("plugins//Managed-" + projectId + ".jar"));
+                .execute().body().byteStream(), targetFile);
 
         runSync(() -> {
             try {
-                Bukkit.getPluginManager().loadPlugin(new File("plugins//Managed-" + projectId + ".jar"));
+                Bukkit.getPluginManager().loadPlugin(targetFile);
             } catch (Exception e) {
-                FileUtils.deleteQuietly(new File("plugins//Managed-" + projectId + ".jar"));
-                response.code(400).json("message=\"The item with the id '" + projectId
-                                + "' is not a valid plugin\"", "error=\"" + e.getMessage() + "\"");
+                FileUtils.deleteQuietly(targetFile);
+                response.code(400).json("message=\"The plugin '" + pluginName
+                                + "' is not valid\"", "error=\"" + e.getMessage() + "\"");
                 return;
             }
-            response.message("The item with the id '" + projectId + "' has been installed");
+            response.message("Plugin '" + pluginName + "' has been installed successfully");
         });
     }
 }
